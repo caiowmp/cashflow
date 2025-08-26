8a2746f9a130d0dc1945c07d5c32d75f459abe36
caiomp <caiomp@ufba.br>
2025-08-26 15:25:06 -0300

    feat: add `data acess` in `Infraestructure` project

---
+++ src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
@@ -0,0 +1,17 @@
+﻿using CashFlow.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CashFlow.Infrastructure.DataAccess
+{
+  public class CashFlowDbContext : DbContext
+  {
+    public DbSet<Expense> Expenses { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      var connectionString = "Server=localhost;Database=cashflowdb;Uid=root;Pwd=default;";
+      var serverVersion = new MySqlServerVersion(new Version(8, 0, 43));
+      optionsBuilder.UseMySql(connectionString, serverVersion);
+    }
+  }
+}
