378c278dc474341e27541de7d7f6a220b58df767
caiomp <caiomp@ufba.br>
2025-08-28 21:03:19 -0300

    feat: add not found custom exception

--- src/CashFlow.Application/UseCases/Expenses/GetById/GetExpenseByIdUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/GetById/GetExpenseByIdUseCase.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
 using CashFlow.Communication.Responses;
 using CashFlow.Domain.Repositories.Expenses;
+using CashFlow.Exception;
+using CashFlow.Exception.ExceptionsBase;
 
 namespace CashFlow.Application.UseCases.Expenses.GetById
 {
@@ -17,7 +19,8 @@ namespace CashFlow.Application.UseCases.Expenses.GetById
 
     public async Task<ResponseExpenseJson> Execute(long id)
     {
-      var result = await _expensesRepository.GetById(id);
+      var result = await _expensesRepository.GetById(id) ?? 
+        throw new NotFoundException(ResourceErrorMessages.EXPENSE_NOT_FOUND);
 
       return _mapper.Map<ResponseExpenseJson>(result);
     }
--- src/CashFlow.Exception/ExceptionsBase/CashFlowException.cs
+++ src/CashFlow.Exception/ExceptionsBase/CashFlowException.cs
@@ -2,6 +2,7 @@
 {
   public abstract class CashFlowException : SystemException
   {
-
+    protected CashFlowException() { }
+    protected CashFlowException(string message) : base(message) { }
   }
 }
