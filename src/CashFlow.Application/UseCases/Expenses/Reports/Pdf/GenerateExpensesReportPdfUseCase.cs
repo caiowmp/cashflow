cdf328d4caba47e7a65cf282c614a9dd854e9cf2
caiomp <caiomp@ufba.br>
2025-09-04 11:43:56 -0300

    feat: add page for the document

--- src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/GenerateExpensesReportPdfUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/GenerateExpensesReportPdfUseCase.cs
@@ -26,6 +26,7 @@ namespace CashFlow.Application.UseCases.Expenses.Reports.Pdf
         return [];
 
       var document = CreateDocument(month);
+      var page = CreatePage(document);
 
       return [];
     }
@@ -41,6 +42,20 @@ namespace CashFlow.Application.UseCases.Expenses.Reports.Pdf
 
 
       return document;
+    }
+  
+    private Section CreatePage(Document document)
+    {
+      var section = document.AddSection();
+      section.PageSetup = document.DefaultPageSetup.Clone();
+
+      section.PageSetup.PageFormat = PageFormat.A4;
+      section.PageSetup.LeftMargin = 40;
+      section.PageSetup.RightMargin = 40;
+      section.PageSetup.TopMargin = 80;
+      section.PageSetup.BottomMargin = 80;
+
+      return section;
     }
   }
 }
