447b1e988179fa21a3e0aec4d1a9469146d6e1fd
caiomp <caiomp@ufba.br>
2025-09-03 21:01:12 -0300

    feat: create excel file and set the fonts

---
+++ src/CashFlow.Application/UseCases/Expenses/Reports/Excel/GenerateExpensesReportExcelUseCase.cs
@@ -0,0 +1,19 @@
+﻿
+using ClosedXML.Excel;
+
+namespace CashFlow.Application.UseCases.Expenses.Reports.Excel
+{
+  public class GenerateExpensesReportExcelUseCase : IGenerateExpensesReportExcelUseCase
+  {
+    public async Task<byte[]> Execute(DateOnly month)
+    {
+      var workbook = new XLWorkbook();
+
+      workbook.Author = "Caio Miranda Pereira";
+      workbook.Style.Font.FontSize = 12;
+      workbook.Style.Font.FontName = "Arial";
+
+      var worksheet = workbook.Worksheets.Add(month.ToString("Y"));
+    }
+  }
+}
