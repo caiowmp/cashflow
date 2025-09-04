cb4aa4dc8c5ef3e453be4efd9001ada9b0656b6f
caiomp <caiomp@ufba.br>
2025-09-04 11:39:46 -0300

    feat: add document

--- src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/GenerateExpensesReportPdfUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/GenerateExpensesReportPdfUseCase.cs
@@ -1,5 +1,7 @@
 ﻿using CashFlow.Application.UseCases.Expenses.Reports.Pdf.Fonts;
+using CashFlow.Domain.Reports;
 using CashFlow.Domain.Repositories.Expenses;
+using MigraDoc.DocumentObjectModel;
 using PdfSharp.Fonts;
 
 namespace CashFlow.Application.UseCases.Expenses.Reports.Pdf
@@ -14,7 +16,7 @@ namespace CashFlow.Application.UseCases.Expenses.Reports.Pdf
       _repository = repository;
 
       GlobalFontSettings.FontResolver = new ExpensesReportFontResolver();
-    } 
+    }
 
     public async Task<byte[]> Execute(DateOnly month)
     {
@@ -23,7 +25,22 @@ namespace CashFlow.Application.UseCases.Expenses.Reports.Pdf
       if (expenses.Count == 0)
         return [];
 
+      var document = CreateDocument(month);
+
       return [];
+    }
+
+    private Document CreateDocument(DateOnly month) 
+    {
+      var document = new Document();
+      document.Info.Title = $"{ResourceReportGenerationMessages.EXPENSES_FOR} {month:Y}";
+      document.Info.Author = "Caio Miranda Pereira";
+
+      var style = document.Styles["Normal"];
+      style!.Font.Name = FontHelper.DEFAULT_FONT;
+
+
+      return document;
     }
   }
 }
