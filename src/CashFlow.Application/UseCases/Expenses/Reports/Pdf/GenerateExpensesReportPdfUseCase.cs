c1741f5788ffa7aa1cf1fcb1a9a7061d339bb708
caiomp <caiomp@ufba.br>
2025-09-04 10:38:13 -0300

    feat: add endpoint and usecase to generate pdf report

---
+++ src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/GenerateExpensesReportPdfUseCase.cs
@@ -0,0 +1,19 @@
+﻿using CashFlow.Domain.Repositories.Expenses;
+
+namespace CashFlow.Application.UseCases.Expenses.Reports.Pdf
+{
+  public class GenerateExpensesReportPdfUseCase(IExpensesReadOnlyRepository repository) : IGenerateExpensesReportPdfUseCase
+  {
+    private const string CURRENCY_SYMBOL = "$";
+
+    public async Task<byte[]> Execute(DateOnly month)
+    {
+      var expenses = await repository.FilterByMonth(month);
+
+      if (expenses.Count == 0)
+        return [];
+
+      return [];
+    }
+  }
+}
