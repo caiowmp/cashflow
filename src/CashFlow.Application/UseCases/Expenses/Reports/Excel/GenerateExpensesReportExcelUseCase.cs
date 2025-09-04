94758164c43d874db5ae51a24bbfa0e173e168b6
caiomp <caiomp@ufba.br>
2025-09-03 22:04:17 -0300

    feat: add expenses info into the excel file

--- src/CashFlow.Application/UseCases/Expenses/Reports/Excel/GenerateExpensesReportExcelUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Reports/Excel/GenerateExpensesReportExcelUseCase.cs
@@ -1,4 +1,5 @@
 ﻿
+using CashFlow.Domain.Enums;
 using CashFlow.Domain.Reports;
 using CashFlow.Domain.Repositories.Expenses;
 using ClosedXML.Excel;
@@ -23,10 +24,35 @@ namespace CashFlow.Application.UseCases.Expenses.Reports.Excel
 
       InsertHeader(worksheet);
 
+      var line = 2;
+      foreach (var expense in expenses)
+      {
+        worksheet.Cell($"A{line}").Value = expense.Title;
+        worksheet.Cell($"B{line}").Value = expense.Date;
+        worksheet.Cell($"C{line}").Value = ConvertPaymentType(expense.PaymentType);
+        worksheet.Cell($"D{line}").Value = expense.Amount;
+        worksheet.Cell($"E{line}").Value = expense.Description;
+
+        line++;
+      }
+
+
       var file = new MemoryStream();
       workbook.SaveAs(file);
 
       return file.ToArray();
+    }
+
+    private string ConvertPaymentType(PaymentType payment)
+    {
+      return payment switch
+      {
+        PaymentType.Cash => ResourceReportGenerationMessages.PAYMENT_TYPE_CASH,
+        PaymentType.CreditCard => ResourceReportGenerationMessages.PAYMENT_TYPE_CREDIT_CARD,
+        PaymentType.DebitCard => ResourceReportGenerationMessages.PAYMENT_TYPE_DEBIT_CARD,
+        PaymentType.EletronicTransfer => ResourceReportGenerationMessages.PAYMENT_TYPE_ELETRONIC_TRANSFER,
+        _ => string.Empty,
+      };
     }
 
     private void InsertHeader(IXLWorksheet worksheet)
@@ -34,7 +60,7 @@ namespace CashFlow.Application.UseCases.Expenses.Reports.Excel
       worksheet.Cell("A1").Value = ResourceReportGenerationMessages.TITLE;
       worksheet.Cell("B1").Value = ResourceReportGenerationMessages.DATE;
       worksheet.Cell("C1").Value = ResourceReportGenerationMessages.PAYMENT_TYPE;
-      worksheet.Cell("D1").Value = ResourceReportGenerationMessages.AMOUNT1;
+      worksheet.Cell("D1").Value = ResourceReportGenerationMessages.AMOUNT;
       worksheet.Cell("E1").Value = ResourceReportGenerationMessages.DESCRIPTION;
 
       worksheet.Cells("A1:E1").Style.Font.Bold = true;
--- src/CashFlow.Domain/Reports/ResourceReportGenerationMessages.Designer.cs
+++ src/CashFlow.Domain/Reports/ResourceReportGenerationMessages.Designer.cs
@@ -63,9 +63,9 @@ namespace CashFlow.Domain.Reports {
         /// <summary>
         ///   Looks up a localized string similar to Amount.
         /// </summary>
-        public static string AMOUNT1 {
+        public static string AMOUNT {
             get {
-                return ResourceManager.GetString("AMOUNT1", resourceCulture);
+                return ResourceManager.GetString("AMOUNT", resourceCulture);
             }
         }
         
@@ -93,6 +93,42 @@ namespace CashFlow.Domain.Reports {
         public static string PAYMENT_TYPE {
             get {
                 return ResourceManager.GetString("PAYMENT_TYPE", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Cash.
+        /// </summary>
+        public static string PAYMENT_TYPE_CASH {
+            get {
+                return ResourceManager.GetString("PAYMENT_TYPE_CASH", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Credit Card.
+        /// </summary>
+        public static string PAYMENT_TYPE_CREDIT_CARD {
+            get {
+                return ResourceManager.GetString("PAYMENT_TYPE_CREDIT_CARD", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Debit Card.
+        /// </summary>
+        public static string PAYMENT_TYPE_DEBIT_CARD {
+            get {
+                return ResourceManager.GetString("PAYMENT_TYPE_DEBIT_CARD", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Eletronic Transfer.
+        /// </summary>
+        public static string PAYMENT_TYPE_ELETRONIC_TRANSFER {
+            get {
+                return ResourceManager.GetString("PAYMENT_TYPE_ELETRONIC_TRANSFER", resourceCulture);
             }
         }
         
