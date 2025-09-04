0aa2d9c8e1863e13ccf286c33dcfed063d660142
caiomp <caiomp@ufba.br>
2025-09-04 11:22:52 -0300

    feat: start font resolver

---
+++ src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/Fonts/FontHelper.cs
@@ -0,0 +1,10 @@
+﻿namespace CashFlow.Application.UseCases.Expenses.Reports.Pdf.Fonts
+{
+  public class FontHelper
+  {
+    public const string MONTSERRAT_BLACK = "Montserrat-Black";
+    public const string MONTSERRAT_REGULAR = "Montserrat-Regular";
+    public const string OPENSANS_BOLD = "OpenSans-Bold";
+    public const string OPENSANS_REGULAR = "OpenSans-Regular";
+  }
+}
--- src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/GenerateExpensesReportPdfUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/GenerateExpensesReportPdfUseCase.cs
@@ -1,14 +1,24 @@
-﻿using CashFlow.Domain.Repositories.Expenses;
+﻿using CashFlow.Application.UseCases.Expenses.Reports.Pdf.Fonts;
+using CashFlow.Domain.Repositories.Expenses;
+using PdfSharp.Fonts;
 
 namespace CashFlow.Application.UseCases.Expenses.Reports.Pdf
 {
-  public class GenerateExpensesReportPdfUseCase(IExpensesReadOnlyRepository repository) : IGenerateExpensesReportPdfUseCase
+  public class GenerateExpensesReportPdfUseCase : IGenerateExpensesReportPdfUseCase
   {
     private const string CURRENCY_SYMBOL = "$";
+    private readonly IExpensesReadOnlyRepository _repository;
+
+    public GenerateExpensesReportPdfUseCase(IExpensesReadOnlyRepository repository)
+    {
+      _repository = repository;
+
+      GlobalFontSettings.FontResolver = new ExpensesReportFontResolver();
+    } 
 
     public async Task<byte[]> Execute(DateOnly month)
     {
-      var expenses = await repository.FilterByMonth(month);
+      var expenses = await _repository.FilterByMonth(month);
 
       if (expenses.Count == 0)
         return [];
