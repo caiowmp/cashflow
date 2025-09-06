05df061ef59a44b7c721813cd5ee5f0c11107840
caiomp <caiomp@ufba.br>
2025-09-06 14:00:43 -0300

    feat: start flow to register user

---
+++ src/CashFlow.Application/UseCases/Users/Register/RegisterUserUseCase.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.CompilerServices;
+using AutoMapper;
+using CashFlow.Communication.Requests;
+using CashFlow.Communication.Responses;
+using CashFlow.Domain.Entities;
+using CashFlow.Exception.ExceptionsBase;
+
+namespace CashFlow.Application.UseCases.Users.Register
+{
+  public class RegisterUserUseCase(IMapper _mapper) : IRegisterUserUseCase
+  {
+    public async Task<ResponseRegisteredUserJson> Execute(RequestRegisterUserJson request)
+    {
+      Validate(request);
+
+      var user = _mapper.Map<User>(request);
+
+      return new ResponseRegisteredUserJson
+      {
+        Name = user.Name,
+      };
+    }
+
+    public void Validate(RequestRegisterUserJson request)
+    {
+      var result = new RegisterUserValidator().Validate(request);
+
+      if (!result.IsValid)
+      {
+        var errorMessages = result.Errors.Select(f => f.ErrorMessage).ToList();
+
+        throw new ErrorOnValidationException(errorMessages);
+      }
+    }
+  }
+}
