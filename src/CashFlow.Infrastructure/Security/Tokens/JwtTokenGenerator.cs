2fdf97e18bf1de93efa2932aa1b85df6741fbb0e
caiomp <caiomp@ufba.br>
2025-09-07 13:12:11 -0300

    feat: generate jwt token

---
+++ src/CashFlow.Infrastructure/Security/Tokens/JwtTokenGenerator.cs
@@ -0,0 +1,41 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using CashFlow.Domain.Entities;
+using CashFlow.Domain.Security.Tokens;
+using Microsoft.IdentityModel.Tokens;
+
+namespace CashFlow.Infrastructure.Security.Tokens
+{
+  public class JwtTokenGenerator(uint _expirationTimeMinutes, string _signingKey) : IAcessTokenGenerator
+  {
+    public string Generate(User user)
+    {
+      var calims = new List<Claim>()
+      {
+        new Claim(ClaimTypes.Name, user.Name),
+        new Claim(ClaimTypes.Sid, user.UserIdIdentifier.ToString()),
+      };
+
+      var tokenDescription = new SecurityTokenDescriptor
+      {
+        Expires = DateTime.UtcNow.AddMinutes(_expirationTimeMinutes),
+        SigningCredentials = new SigningCredentials(GetSecurityKey(), SecurityAlgorithms.HmacSha256Signature), 
+        Subject = new ClaimsIdentity()
+      };
+
+      var tokenHandler = new JwtSecurityTokenHandler();
+
+      var securityToken = tokenHandler.CreateToken(tokenDescription);
+
+      return tokenHandler.WriteToken(securityToken);
+    }
+
+    private SymmetricSecurityKey GetSecurityKey()
+    {
+      var key = Encoding.UTF8.GetBytes(_signingKey);
+
+      return new SymmetricSecurityKey(key);
+    }
+  }
+}
