c7e02630f7fd2c3c49295679962ae831d7e205dc
caiomp <caiomp@ufba.br>
2025-09-08 10:43:26 -0300

    feat: conclue token test

--- src/CashFlow.Infrastructure/DependencyInjectionExtension.cs
+++ src/CashFlow.Infrastructure/DependencyInjectionExtension.cs
@@ -2,9 +2,11 @@
 using CashFlow.Domain.Repositories.Expenses;
 using CashFlow.Domain.Repositories.User;
 using CashFlow.Domain.Security.Cryptography;
+using CashFlow.Domain.Security.Tokens;
 using CashFlow.Infrastructure.DataAccess;
 using CashFlow.Infrastructure.DataAccess.Repositories;
 using CashFlow.Infrastructure.DataAccess.Repositories.User;
+using CashFlow.Infrastructure.Security.Tokens;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -16,9 +18,18 @@ namespace CashFlow.Infrastructure
     public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
       AddDbContext(services, configuration);
+      AddToken(services, configuration);
       AddRepositories(services);
 
       services.AddScoped<IPasswordEncripter, Security.Cryptography.BCrypt>();
+    }
+
+    private static void AddToken(IServiceCollection services, IConfiguration configuration)
+    {
+      var expirationTimeMinutes = configuration.GetValue<uint>("Settings:Jwt:ExpireMinutes");
+      var signingKey = configuration.GetValue<string>("Settings:Jwt:SigningKey");
+
+      services.AddScoped<IAcessTokenGenerator>(config => new JwtTokenGenerator(expirationTimeMinutes, signingKey!));
     }
 
     private static void AddRepositories(IServiceCollection services)
