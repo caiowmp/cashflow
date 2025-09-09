d4f04ac2c1177b22538f31e6e6ceb65c74615972
caiomp <caiomp@ufba.br>
2025-09-09 18:55:22 -0300

    test: start implementation using mocks

---
+++ tests/CommonTestUtilities/Repositories/UnitOfWorkBuilder.cs
@@ -0,0 +1,15 @@
+﻿using CashFlow.Domain.Repositories;
+using Moq;
+
+namespace CommonTestUtilities.Repositories
+{
+  public class UnitOfWorkBuilder
+  {
+    public static IUnityOfWork Build()
+    {
+      var mock = new Mock<IUnityOfWork>();
+
+      return mock.Object;
+    }
+  }
+}
---
+++ tests/UseCases.Test/Users/Register/UserWriteOnlyRepositoryBuilder.cs
@@ -0,0 +1,15 @@
+﻿using CashFlow.Domain.Repositories.User;
+using Moq;
+
+namespace UseCases.Test.Users.Register
+{
+  public class UserWriteOnlyRepositoryBuilder
+  {
+    public static IUserWriteOnlyRepository Build()
+    {
+      var mock = new Mock<IUserWriteOnlyRepository>();
+
+      return mock.Object;
+    }
+  }
+}
