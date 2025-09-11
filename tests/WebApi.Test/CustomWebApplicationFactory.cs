fb6c7b199bc61a8de028acec9e8e4ffecfbfb3c1
caiomp <caiomp@ufba.br>
2025-09-10 23:31:23 -0300

    test: add integration test for login

--- tests/WebApi.Test/CustomWebApplicationFactory.cs
+++ tests/WebApi.Test/CustomWebApplicationFactory.cs
@@ -1,4 +1,7 @@
-﻿using CashFlow.Infrastructure.DataAccess;
+﻿using CashFlow.Domain.Entities;
+using CashFlow.Domain.Security.Cryptography;
+using CashFlow.Infrastructure.DataAccess;
+using CommonTestUtilities.Entities;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
@@ -8,6 +11,9 @@ namespace WebApi.Test
 {
   public class CustomWebApplicationFactory : WebApplicationFactory<Program>
   {
+    private User _user;
+    private string _password;
+
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
       builder.UseEnvironment("Test")
@@ -20,7 +26,29 @@ namespace WebApi.Test
             ConfigureWebHost.UseInMemoryDatabase("InMmemoryDbForTesting");
             ConfigureWebHost.UseInternalServiceProvider(provider);
           });
+
+          var scope = services.BuildServiceProvider().CreateScope();
+          var dbContext = scope.ServiceProvider.GetRequiredService<CashFlowDbContext>();
+          var passwordEncrypter = scope.ServiceProvider.GetRequiredService<IPasswordEncripter>();
+
+          SatarDatabase(dbContext, passwordEncrypter);
         });
+    }
+
+    public string GetEmail() => _user.Email;
+    public string GetName() => _user.Name;
+    public string GetPassword() => _password;
+
+    private void SatarDatabase(CashFlowDbContext dbContext, IPasswordEncripter passwordEncrypter)
+    {
+      _user = UserBuilder.Build();
+      _password = _user.Password;
+
+      _user.Password = passwordEncrypter.Encrypt(_user.Password);
+
+      dbContext.Users.Add(_user);
+
+      dbContext.SaveChanges();  
     }
   }
 }
