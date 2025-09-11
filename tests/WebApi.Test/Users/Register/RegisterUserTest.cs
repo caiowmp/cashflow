cd418007e9ea426ca1f7b50ba4c2491f3a56da95
caiomp <caiomp@ufba.br>
2025-09-10 22:39:06 -0300

    test: writes test for error on register user

--- tests/WebApi.Test/Users/Register/RegisterUserTest.cs
+++ tests/WebApi.Test/Users/Register/RegisterUserTest.cs
@@ -1,6 +1,7 @@
 ﻿using System.Net;
 using System.Net.Http.Json;
 using System.Text.Json;
+using CashFlow.Exception;
 using CommonTestUtilities.Requests;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc.Testing;
@@ -33,6 +34,25 @@ namespace WebApi.Test.Users.Register
 
       response.RootElement.GetProperty("name").GetString().Should().Be(request.Name);
       response.RootElement.GetProperty("token").GetString().Should().NotBeNullOrEmpty();
+    }
+
+    [Fact]
+    public async Task Error_Empty_Name()
+    {
+      var request = RequestRegisterUserJsonBuilder.Build();
+      request.Name = string.Empty;
+
+      var result = await _httpClient.PostAsJsonAsync(METHOD, request);
+
+      result.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+
+      var body = await result.Content.ReadAsStreamAsync();
+
+      var response = await JsonDocument.ParseAsync(body);
+
+      var errors = response.RootElement.GetProperty("errorMessages").EnumerateArray();
+
+      errors.Should().HaveCount(1).And.Contain(error => error.GetString()!.Equals(ResourceErrorMessages.NAME_EMPTY));
     }
   }
 }
