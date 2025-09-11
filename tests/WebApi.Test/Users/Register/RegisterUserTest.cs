3bd074f5b2fd04fa637a601e0d2a973c52ae1918
caiomp <caiomp@ufba.br>
2025-09-10 22:16:14 -0300

    test: first integration test

--- tests/WebApi.Test/Users/Register/RegisterUserTest.cs
+++ tests/WebApi.Test/Users/Register/RegisterUserTest.cs
@@ -1,13 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc.Testing;
+﻿using System.Net;
+using System.Net.Http.Json;
+using CommonTestUtilities.Requests;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
 
 namespace WebApi.Test.Users.Register
 {
-  public class RegisterUserTest : IClassFixture<WebApplicationFactory<Program>>
+  public class RegisterUserTest : IClassFixture<CustomWebApplicationFactory>
   {
+    private const string METHOD = "api/User";
+
+    private readonly HttpClient _httpClient;
+
+    public RegisterUserTest(CustomWebApplicationFactory webApplicationFactory)
+    {
+      _httpClient = webApplicationFactory.CreateClient();
+    }
+
     [Fact]
     public async Task Sucess()
     {
+      var request = RequestRegisterUserJsonBuilder.Build();
 
+      var result = await _httpClient.PostAsJsonAsync(METHOD, request);
+
+      result.StatusCode.Should().Be(HttpStatusCode.Created);
     }
   }
 }
