c8fad3ba454dd016e6fde5b00b511e7c6bb9432c
caiomp <caiomp@ufba.br>
2025-09-23 22:14:45 -0300

    feat: adds routine for update user profile

--- src/CashFlow.Infrastructure/DataAccess/Repositories/UserRepository.cs
+++ src/CashFlow.Infrastructure/DataAccess/Repositories/UserRepository.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace CashFlow.Infrastructure.DataAccess.Repositories
 {
-  internal class UserRepository(CashFlowDbContext _dbContext) : IUserReadOnlyRepository, IUserWriteOnlyRepository
+  internal class UserRepository(CashFlowDbContext _dbContext) : IUserReadOnlyRepository, IUserWriteOnlyRepository, IUserUpdateOnlyRepository
   {
     public async Task Add(User user)
     {
@@ -16,9 +16,19 @@ namespace CashFlow.Infrastructure.DataAccess.Repositories
       return await _dbContext.Users.AnyAsync(user => user.Email.Equals(email));
     }
 
+    public async Task<User> GetById(long id)
+    {
+      return await _dbContext.Users.FirstAsync(user => user.Id == id);
+    }
+
     public async Task<User?> GetUserByEmail(string email)
     {
       return await _dbContext.Users.AsNoTracking().FirstOrDefaultAsync(user => user.Email.Equals(email));
+    }
+
+    public void Update(User user)
+    {
+      _dbContext.Users.Update(user);
     }
   }
 }
